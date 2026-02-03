a9c91a48890ef2d7cb2a7f22d5ee2f0be9ee7dc5
alopez2397 <alopez2397@gmail.com>
2026-02-03 18:14:44 +0100

    Fix database connection parameters

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 class Database {
     public static function conectar() {
-        $db = new mysqli("94.76.251.66:3308", "Administrador", "8915452@jll", "rapidgest");
+        $db = new mysqli("94.76.251.66", "Administrador", "8915452@jll", "rapidgest", 3308);
         if ($db->connect_error) {
             die("Error de conexión");
         }
